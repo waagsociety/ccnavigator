a6e89b407d6270178dc26fc083d2f0be774a944a
Alain Otjens <alain@waag.org>
2018-05-15 17:07:06 +0200

    filter styling

--- frontend/react/ccnavigator/src/containers/ToolFilter.js
+++ frontend/react/ccnavigator/src/containers/ToolFilter.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 class ToolFilter extends React.Component {
 
-
   render() {
-    var filters = {
-      groupSize: ["0-5", "5-10", "10-25", "25+"],
-      duration: ["0-5 min", "5-30 min", "30-120 min", "2-4 hr", "full day+"],
-      facilitatorParticipant: ["1/1", "1/5", "1/10", "1/25"],
-      experienceLevelFacilitator: ["1", "2", "3", "4"],
-    }
 
     return (
       <div className="tool-filters">
@@ -22,6 +15,37 @@ class ToolFilter extends React.Component {
             <li>5-10</li>
             <li>10-25</li>
             <li>25+</li>
+          </ul>
+        </div>
+
+        <div className="tool-filter">
+          <h4>duration</h4>
+          <ul className="tool-filter-options">
+            <li>0-5 min</li>
+            <li>5-30 min</li>
+            <li className="selected">30-120 min</li>
+            <li>2-4 hr</li>
+            <li>full day+</li>
+          </ul>
+        </div>
+
+        <div className="tool-filter">
+          <h4>facilitator / participant</h4>
+          <ul className="tool-filter-options">
+            <li>1/1</li>
+            <li>1/5</li>
+            <li>1/10</li>
+            <li>1/25</li>
+          </ul>
+        </div>
+
+        <div className="tool-filter">
+          <h4>experience level facilitator</h4>
+          <ul className="tool-filter-options">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
           </ul>
         </div>
       </div>
